b6132241500c6afcd011f52252d37711ebbeb6e3
Shashank Taliwal <shashanktaliwal07@gmail.com>
2022-04-02 23:00:33 +0530

    Changes ho jaayenge

--- src/components/Card1/index.js
+++ src/components/Card1/index.js
@@ -1,34 +1,46 @@
 import './styles.css';
 
-//----------
-//Components
-//----------
-
 function Card1(props) {
-      return(
-          <div className="card">
-            
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let vehicleNumber = event.target.elements[0].value;
+    const dataObj = {
+      vehicleNumber: vehicleNumber,
+    };
+    await fetch(`http://localhost:8080/${vehicleNumber}`, {
+      method: 'GET',
+      headers: { "Content-type": "application/json" },
+      mode: "no-cors"
+    }).then(Response => console.log(Response))
+    .catch(Error => console.log(Error));
+  }
+  return (
+    <div className="card">
 
-            <h1>Vehicle Exit</h1>
-            <div>
-            <h4>vehicle Number:</h4>
-            <input type="text" id="fname" name="fname" placeholder='Enter Vehicle Number'/>
-            <br/>
-            </div>
-            
-            <input type="submit" value="Submit" class="button"/>
 
-            <div>
-            <h4>Calculated Fare:</h4>
-            <p>100</p>
-            <br/>
-            </div>
+      <h1>Vehicle Exit</h1>
+      <div>
+        <h4>vehicle Number:</h4>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" id="fname" name="fname" placeholder='Enter Vehicle Number' />
+          <br />
+        </div>
 
-            
+        <input type="submit" value="Submit" class="button" />
+      </form>
+      <div>
+        <h4>Calculated Fare:</h4>
+        <p>100</p>
+        <br />
+      </div>
 
-           
-          </div>
-      );
-    }
 
-    export default Card1;
+
+
+    </div>
+  );
+}
+
+export default Card1;
